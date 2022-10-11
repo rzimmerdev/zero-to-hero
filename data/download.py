ae1ea8a737495c85d2e0597eb7f276618a6bf11c
Rafael Zimmer <rzimmerdev@gmail.com>
2022-10-11 16:49:16 -0300

    Added boilerplate project structure

---
+++ data/download.py
@@ -0,0 +1,16 @@
+# Script to automatically download and cache dataset
+# Usage: python download.py
+#
+# To learn more about the dataset, access:
+# https://www.cityscapes-dataset.com/
+import os
+import sys
+
+
+# Download and cache dataset
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
