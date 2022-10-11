43002bef89afe2385ba8dd51411d010154b3770a
Rafael Zimmer <rzimmerdev@gmail.com>
2022-10-11 17:12:11 -0300

    Option to install cityscrapes dataset download script, as well as requirements file

--- data/download.py
+++ data/download.py
@@ -5,11 +5,29 @@
 # https://www.cityscapes-dataset.com/
 import os
 import sys
+import pip
 
 
 # Download and cache dataset
 def main():
     pass
+
+
+def download(name='cityscapes', path='data/downloads'):
+    """Select one of the available and implemented datasets to download:
+    name=any(['cityscapes', 'camvid', 'labelme'])
+    """
+    if name == 'cityscapes':
+        download_cityscapes(path)
+    else:
+        raise NotImplementedError
+
+
+def download_cityscapes(path='data/downloads'):
+    if hasattr(pip, 'main'):
+        pip.main(['install', 'cityscrapesscripts'])
+    else:
+        raise EnvironmentError("pip is not installed")
 
 
 if __name__ == "__main__":
