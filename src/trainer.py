22621030317cac7c1c47b8742f1fd3ba2f47acda
Rafael Zimmer <rzimmerdev@gmail.com>
2022-12-25 04:53:01 -0300

    feature: fixed training loop arguments for Lightning module

--- src/trainer.py
+++ src/trainer.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
-import torch.optim
+import torch
+from torch import nn, optim
 import pytorch_lightning as pl
 
 
 class LitTrainer(pl.LightningModule):
-    def __init__(self, model, loss_fn, optim):
+    def __init__(self, model):
         super().__init__()
         self.model = model
-        self.loss_fn = loss_fn
-        self.optim = optim
+        self.optim = optim.Adam(self.parameters(), lr=1e-4)
+        self.loss = nn.CrossEntropyLoss()
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        x = x.to(torch.float32)
 
         y_pred = self.model(x).reshape(1, -1)
-        train_loss = self.loss_fn(y_pred, y)
+        train_loss = self.loss(y_pred, y)
 
         self.log("train_loss", train_loss)
         return train_loss
@@ -24,22 +24,23 @@ class LitTrainer(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         # this is the validation loop
         x, y = batch
-        x = x.to(torch.float32)
 
         y_pred = self.model(x).reshape(1, -1)
-        validate_loss = self.loss_fn(y_pred, y)
+        validate_loss = self.loss(y_pred, y)
 
         self.log("val_loss", validate_loss)
 
     def test_step(self, batch, batch_idx):
         # this is the test loop
         x, y = batch
-        x = x.to(torch.float32)
 
         y_pred = self.model(x).reshape(1, -1)
-        test_loss = self.loss_fn(y_pred, y)
+        test_loss = self.loss(y_pred, y)
 
         self.log("test_loss", test_loss)
+
+    def forward(self, x):
+        return self.model(x)
 
     def configure_optimizers(self):
         return self.optim
